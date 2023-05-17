ba55ea05cd1c0fda43026af5c7c585947a406682
Nathan Carqueijeiro <nathancarqueijeiroetd@gmail.com>
2023-05-17 09:22:08 +0200

    ajout de graphliste

---
+++ projet/src/GrapheListe.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class GrapheListe implements Graphe {
+    /**
+     * @return
+     */
+    @Override
+    public List<String> listeNoeud() {
+        return null;
+    }
+
+    /**
+     * @param n
+     * @return
+     */
+    @Override
+    public List<String> suivant(String n) {
+        return null;
+    }
+}
