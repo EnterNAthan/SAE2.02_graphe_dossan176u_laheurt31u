002d255d2d00a73afd18da49eeee5ca3a3ab8d34
Nathan Carqueijeiro <nathancarqueijeiroetd@gmail.com>
2023-05-22 11:02:18 +0200

    ajoutez arc

--- projet/src/GrapheListe.java
+++ projet/src/GrapheListe.java
@@ -30,24 +30,17 @@ public class GrapheListe implements Graphe {
     public void ajouterArc(String depart, String destination, double cout) throws Exception {
         if(!ensNom.contains(depart)){
             Noeud n = new Noeud(depart);
-            Noeud n2 = new Noeud(destination);
             ensNom.add(depart);
             ensNoeuds.add(n);
-            ensNoeuds.add(n2);
         }
         if(!ensNom.contains(destination)){
-            Noeud n = new Noeud(depart);
             Noeud n2 = new Noeud(destination);
             ensNom.add(destination);
-            ensNoeuds.add(n);
             ensNoeuds.add(n2);
         }
         for (Noeud noeud : ensNoeuds) {
             if(noeud.getNom().equals(depart)){
                 noeud.ajouterArc(depart, cout);
-            }
-            else if(noeud.getNom().equals(destination)){
-                noeud.ajouterArc(destination, cout);
             }
         }
     }
