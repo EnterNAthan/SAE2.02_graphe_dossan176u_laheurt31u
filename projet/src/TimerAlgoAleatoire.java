411df04a6cf22fec5e886b4f7598052c9a38b374
raphleboss <raphaellaheurte@gmail.com>
2023-05-26 00:18:49 +0200

    debut des mesures d'algo aleatoires

--- projet/src/TimerAlgoAleatoire.java
+++ projet/src/TimerAlgoAleatoire.java
@@ -1,10 +1,15 @@
+import java.io.File;
+import java.io.FileWriter;
+
 public class TimerAlgoAleatoire {
     public static void main (String[] args) {
         try {
-
+            File f = new File("Tableau_timers/Graphes_aleatoires");
+            FileWriter monF = new FileWriter("Tableau_timers/Graphes_aleatoires");
             GrapheListe g2 = new GrapheListe();
-            g2.genererGraphe(2,2,2);
+            g2.genererGraphe(2,1,2);
             System.out.println(g2.toString());
+            System.out.println(g2.timerAlgo());
         } catch (Exception e) {
             System.out.println(e);
         }
